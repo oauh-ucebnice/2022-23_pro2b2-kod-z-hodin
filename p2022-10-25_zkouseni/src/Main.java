184c4bcff80807d486d3bd9a450ab523661c235f
Martin <public@e-mis.cz>
2022-10-25 15:26:14 +0200

    pro2b2 2022-10-25 + .gitignore

---
+++ p2022-10-25_zkouseni/src/Main.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String jmeno;
+        LocalDate datum;
+        datum = LocalDate.of(2022,10,25);
+        double delkaStranyA;
+        double delkaStranyB;
+        delkaStranyA = 2.5;
+        delkaStranyB = 5.3;
+    }
+}
